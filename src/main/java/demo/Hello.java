13b669acb161aca47702b61aaa613b43bfb0cc60
restmad <restmad@outlook.com>
2017-11-29 16:23:47 +0800

    111

---
+++ src/main/java/demo/Hello.java
@@ -0,0 +1,15 @@
+package demo;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by MaRui on 2017/11/29.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+
+        Gson gson = new Gson();
+//        gson.fromJson
+    }
+}
---
+++ src/main/java/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by MaRui on 2017/11/29.
+ */
+package demo;
